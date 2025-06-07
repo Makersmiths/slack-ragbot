e5e3c3773bfd5d9604b207658c4242be6ed31099
rtd <rob.t.donahue@gmail.com>
2025-06-07 15:25:14 -0400

    removed extraneous code

--- consolidated_loader.py
+++ consolidated_loader.py
@@ -23,7 +23,6 @@ cosmos_details = get_cosmosdb_details(subscription_id, resource_group)
 #Set environment variables for scraping/loading
 os.environ['OPENAI_EMBEDDING_URI'] = "{}openai/deployments/text_embedding/embeddings/?api-version=2023-05-15".format(cs_details[0])
 os.environ['OPENAI_EMBEDDING_TOKEN'] = retrieve_secret('Azure-OpenAI-embedding-token', keyvault_url)
-os.environ['COSMOS_WRITE_KEY'] = retrieve_secret('cosmos-write-key', keyvault_url)
 os.environ['COSMOS_URI'] = cosmos_details[0]
 os.environ['COSMOS_DB_NAME'] ='vectorDB'
 os.environ['COSMOS_CONTAINER_NAME'] ='vectorContainer'
