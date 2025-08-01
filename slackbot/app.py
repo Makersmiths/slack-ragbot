34de144af110a154f0d55353a802c53b563f9bc7
rtd <rob.t.donahue@gmail.com>
2025-07-31 20:46:03 -0400

    updated deployment uri

--- slackbot/app.py
+++ slackbot/app.py
@@ -17,7 +17,7 @@ cosmos_details = get_cosmosdb_details(subscription_id, resource_group)
 #Set environment variables
 os.environ['SLACK_BOT_TOKEN']=retrieve_secret('slack-bot-token',keyvault_url)
 os.environ['SLACK_APP_TOKEN']=retrieve_secret('slack-app-token',keyvault_url)
-os.environ['OPENAI_GPT_URI']="{}openai/deployments/gpt-4-deployment/chat/completions/?api-version=2023-05-15".format(cs_details[0])
+os.environ['OPENAI_GPT_URI']="{}openai/deployments/gpt-4/chat/completions/?api-version=2025-01-01-preview".format(cs_details[0])
 os.environ['OPENAI_EMBEDDING_URI']="{}openai/deployments/text-embedding-3-large/embeddings/?api-version=2023-05-15".format(cs_details[0])
 os.environ['AZURE_OPENAI_API_KEY']=cs_details[1]
 os.environ['COSMOS_KEY']=cosmos_details[1]
